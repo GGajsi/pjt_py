5106f5554d16094b91c16c09c0ad087fb5e04bb5
GGajsi <danghyeoncheong2@gmail.com>
2026-09-04 16:39:59 +0900

    함수

--- number_binggo.py
+++ number_binggo.py
@@ -1,5 +1,6 @@
-a = ["     *","    ***","   *****","  *******"," *********"]
+lines = 5
 
-for i in a:
-    print(i)
-
+for i in range(1, lines + 1):
+    spaces = " " * (lines - i)
+    stars = "*" * (2 * i - 1)
+    print(spaces + stars)
